86f6f3e1a7e13821b107ee6139ff53d6a2baca23
czqzju <czqzju@gmail.com>
2020-07-11 20:14:12 +1200

    Subsets https://leetcode.com/explore/challenge/card/july-leetcoding-challenge/545/week-2-july-8th-july-14th/3387/

---
+++ July 2020/Subsets.py
@@ -0,0 +1,13 @@
+from itertools import chain, combinations
+
+class Solution(object):
+    def subsets(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[List[int]]
+        """
+        res = map(list, chain.from_iterable(combinations(nums, r) for r in range(len(nums) + 1)))
+        return [x for x in res]
+
+
+print(Solution().subsets([1,2,3]))
