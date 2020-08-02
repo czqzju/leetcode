3289d7fb937ddc532fe59c278e6eb5a1edc1b839
czqzju <czqzju@gmail.com>
2020-08-02 21:04:14 +1200

    Design HashSet https://leetcode.com/explore/challenge/card/august-leetcoding-challenge/549/week-1-august-1st-august-7th/3410/

---
+++ Aug 2020/Design HashSet.py
@@ -0,0 +1,25 @@
+
+class MyHashSet:
+
+    def __init__(self):
+        self.data = set()
+
+    def add(self, key: int) -> None:
+        self.data.add(key)
+
+    def remove(self, key: int) -> None:
+        if key in self.data:
+            self.data.remove(key)
+
+    def contains(self, key: int) -> bool:
+        """
+        Returns true if this set contains the specified element
+        """
+        return key in self.data
+
+# Your MyHashSet object will be instantiated and called as such:
+key = 3
+obj = MyHashSet()
+obj.add(key)
+obj.remove(key)
+param_3 = obj.contains(key)
