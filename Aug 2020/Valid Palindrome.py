bf14f37f5fb8c2ec687d53a32d97a3223b4ac167
czqzju <czqzju@gmail.com>
2020-08-03 19:29:00 +1200

    Valid Palindrome https://leetcode.com/explore/challenge/card/august-leetcoding-challenge/549/week-1-august-1st-august-7th/3411/

---
+++ Aug 2020/Valid Palindrome.py
@@ -0,0 +1,9 @@
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        s2 = ""
+        for i in range(len(s)):
+            if s[i].isalnum():
+                s2 += s[i]
+        return s2.lower() == s2[::-1].lower()
+
+print(Solution().isPalindrome("A man, a plan, a canal: Panama"))
