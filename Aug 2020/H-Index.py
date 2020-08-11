927b4ab877a8983696025718d23d026141bc288e
czqzju <czqzju@gmail.com>
2020-08-11 20:33:38 +1200

    H-Index https://leetcode.com/explore/challenge/card/august-leetcoding-challenge/550/week-2-august-8th-august-14th/3420/

---
+++ Aug 2020/H-Index.py
@@ -0,0 +1,13 @@
+from typing import List
+class Solution:
+    def hIndex(self, citations: List[int]) -> int:
+        citations.sort()
+        i= 0
+        while i < len(citations) and citations[i] < len(citations) - i:
+            i += 1
+        return len(citations) - i
+
+
+
+
+print(Solution().hIndex([3,0,6,1,5]))
